f4d9d544517a3c011eb0470d7de841356d3e91e0
unknown <you.nly.yolo.once@gmail.com>
2020-05-20 15:24:40 +0200

    Python project initialized

---
+++ python/tut_scrape.py
@@ -0,0 +1,24 @@
+import bs4 as bs
+import urllib.request
+import time
+
+
+def read_page(link, filename):
+    f = open(filename, "a")
+    source = urllib.request.urlopen(link).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
+
+    urls = soup.select('.list__item__picture a')
+    for url in urls:
+        output = url.get('href')
+        f.write(output + "\n")
+
+    is_last = 1
+    next_pages = soup.select('.page-step a')
+    for next_page in next_pages:
+        if 'ostatnia' in next_page.get('title'):
+            is_last = 0
+
+    f.close()
+    time.sleep(1)
+    return bool(is_last)
